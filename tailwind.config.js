63c5a1e0ad2060a51ebe28bccd442612dca3159f
stephengilbert1 <stephengilbert1@gmail.com>
2025-06-07 20:47:50 -0700

    Fix merge conflict in package.json

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // tailwind.config.js
 export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts}"],
@@ -16,15 +15,3 @@ export default {
   },
   plugins: [],
 };
-=======
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
->>>>>>> 8e57d5b40fdb306c18ac51772d3d3a7872e2ae5b
